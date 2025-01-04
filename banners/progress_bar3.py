32d45b85a34a1e602afe506cd2902bec2a66d80c
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2025-01-04 21:00:06 +0700

    Update banners

---
+++ banners/progress_bar3.py
@@ -0,0 +1,17 @@
+import time
+import sys
+from colorama import Fore
+
+def hacker_download_animation(duration=5):
+    # ฟังก์ชันสำหรับแสดงการดาวน์โหลดแบบ hacker
+    print(Fore.GREEN + "เริ่มการดาวน์โหลด...")
+
+    # สร้างหน้าต่างดาวน์โหลดแบบ hacker
+    for _ in range(duration):
+        sys.stdout.write(Fore.RED + "\r[███████████████████----] " + str(duration) + "s Remaining...")
+        sys.stdout.flush()
+        time.sleep(1)
+        duration -= 1
+
+    sys.stdout.write(Fore.GREEN + "\r[███████████████████████] " + "ดาวน์โหลดเสร็จสิ้น!\n")
+    sys.stdout.flush()
