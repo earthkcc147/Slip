50b7bc553841069c67a9f8b2052a0bb50d877d6f
earthkcc147 <145793741+earthkcc147@users.noreply.github.com>
2025-01-04 19:08:18 +0700

    Update __init__.py

--- function/__init__.py
+++ function/__init__.py
@@ -2,3 +2,5 @@ from .script import create_fake_receipt
 from .autoupdate import autoupdate_repository
 from .login import login
 from .get import get_full_info 
+
+from .send import smdc, get_current_time, send
